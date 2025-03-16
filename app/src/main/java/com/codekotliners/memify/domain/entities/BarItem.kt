ea961d49ab4e1c81894ccd4567c1b8a65492af0b
yorickyeng <sultanovainur05@gmail.com>
2025-03-16 12:33:54 +0300

    Add BottomNavigationBar with empty screens

--- app/src/main/java/com/codekotliners/memify/domain/entities/BarItem.kt
+++ app/src/main/java/com/codekotliners/memify/domain/entities/BarItem.kt
@@ -1,5 +1,8 @@
 package com.codekotliners.memify.domain.entities
 
 data class BarItem(
-    val title: String, var iconNotPressed: Int, val iconPressed: Int, val route: String
+    val title: String,
+    var iconNotPressed: Int,
+    val iconPressed: Int,
+    val route: String
 )
