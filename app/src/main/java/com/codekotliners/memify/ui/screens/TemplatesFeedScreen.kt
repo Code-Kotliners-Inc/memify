27555991b521e5f089d6abedb81a282492a8f5f5
mareliberum <vladfokin646@gmail.com>
2025-03-18 21:05:54 +0300

    убрал неиспользуемые импорты

--- app/src/main/java/com/codekotliners/memify/ui/screens/TemplatesFeedScreen.kt
+++ app/src/main/java/com/codekotliners/memify/ui/screens/TemplatesFeedScreen.kt
@@ -21,13 +21,11 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.codekotliners.memify.ui.viewmodels.TabState
 import com.codekotliners.memify.ui.viewmodels.Tabs
