f3afcaa6b2ebe1e088440dab189cba03b8ffc17f
mareliberum <vladfokin646@gmail.com>
2025-03-18 23:12:48 +0300

    ci

--- app/src/main/java/com/codekotliners/memify/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/codekotliners/memify/ui/screens/HomeScreen.kt
@@ -13,15 +13,16 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun HomeScreen() {
     Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
+        modifier =
+            Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background),
     ) {
         Text(
             text = "Home Page, fuckers",
             fontSize = 30.sp,
             fontStyle = FontStyle.Italic,
-            color = MaterialTheme.colorScheme.onBackground
+            color = MaterialTheme.colorScheme.onBackground,
         )
     }
 }
