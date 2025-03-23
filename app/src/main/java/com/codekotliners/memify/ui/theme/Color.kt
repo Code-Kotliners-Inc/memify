e3a7a9e06dee11fd38144124f20876c3e49498e5
Fetisov Igor <fetisyony@gmail.com>
2025-03-23 23:42:52 +0300

    Set up color scheme, typography and theme

--- app/src/main/java/com/codekotliners/memify/ui/theme/Color.kt
+++ app/src/main/java/com/codekotliners/memify/ui/theme/Color.kt
@@ -2,10 +2,22 @@ package com.codekotliners.memify.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val primaryLight = Color(0xFF0077FF)
+val onPrimaryLight = Color(0xFFFFFFFF)
+val primaryContainerLight = Color(0xFFFFFFFF)
+val onPrimaryContainerLight = Color(0xFF747474)
+val backgroundLight = Color(0xFFF0F2F5)
+val surfaceLight = Color(0xFFFFFFFF)
+val errorLight = Color(0xFFE60023)
+
+val primaryDark = Color(0xFF0077FF)
+val onPrimaryDark = Color(0xFF000000)
+val primaryContainerDark = Color(0xFF333333)
+val onPrimaryContainerDark = Color(0xFF747474)
+val backgroundDark = Color(0xFF939393)
+val surfaceDark = Color(0xFF333333)
+val errorDark = Color(0xFFE60023)
+
+val likeRedLight = Color(0xFFFF0000)
+val likeRedDark = Color(0xFFB71C1C)
