d183e83b80c08acaabd79abd6643cf7e5736d8bc
Valery <87361814+val3rkq@users.noreply.github.com>
2025-03-29 16:01:43 +0300

    main commit

---
+++ app/src/main/java/com/codekotliners/memify/domain/entities/auth/Response.kt
@@ -0,0 +1,13 @@
+package com.codekotliners.memify.domain.entities.auth
+
+sealed class Response<out T> {
+    data object Loading: Response<Nothing>()
+
+    data class Success<out T>(
+        val data: T
+    ): Response<T>()
+
+    data class Failure<out T>(
+        val error: Exception
+    ): Response<T>()
+}
