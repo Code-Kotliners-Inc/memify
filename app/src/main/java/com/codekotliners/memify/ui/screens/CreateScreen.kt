4e4be1f94c5e1781f8aa6b7f5b04baf18c4ee008
IvanCRA <101933328+IvanCRA@users.noreply.github.com>
2025-03-31 17:53:55 +0300

    add bottom sheet scaffold

--- app/src/main/java/com/codekotliners/memify/ui/screens/CreateScreen.kt
+++ app/src/main/java/com/codekotliners/memify/ui/screens/CreateScreen.kt
@@ -3,25 +3,35 @@ package com.codekotliners.memify.ui.screens
 import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.material3.rememberStandardBottomSheetState
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -44,11 +54,81 @@ import com.codekotliners.memify.ui.theme.MemifyTheme
 fun CreateScreen() {
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
 
-    Scaffold(
-        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        topBar = { CreateScreenTopBar(scrollBehavior) },
+    val minHeight = 800.dp
+    val maxHeight = 900.dp
+
+    val bottomSheetState =
+        rememberStandardBottomSheetState(
+            initialValue = SheetValue.PartiallyExpanded,
+            confirmValueChange = { newValue ->
+                newValue != SheetValue.Hidden
+            },
+        )
+    val scaffoldState = rememberBottomSheetScaffoldState(bottomSheetState = bottomSheetState)
+
+    BottomSheetScaffold(
+        scaffoldState = scaffoldState,
+        sheetContainerColor = MaterialTheme.colorScheme.surface,
+        sheetDragHandle = {
+            Column(
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp)
+                        .height(54.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Icon(
+                    imageVector =
+                        if (bottomSheetState.targetValue == SheetValue.Expanded) {
+                            Icons.Default.KeyboardArrowDown
+                        } else {
+                            Icons.Default.KeyboardArrowUp
+                        },
+                    contentDescription = "Свайп",
+                    modifier = Modifier.size(24.dp),
+                )
+                Text(text = "Выбрать шаблон")
+            }
+        },
+        sheetContent = {
+            Box(
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .heightIn(min = minHeight, max = maxHeight)
+                        .background(MaterialTheme.colorScheme.surface),
+                contentAlignment = Alignment.TopCenter,
+            ) {
+                Column(
+                    modifier = Modifier.padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Text(text = "Здесь был один из котиков-разрабов", textAlign = TextAlign.Center)
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Icon(
+                        imageVector =
+                            if (bottomSheetState.targetValue == SheetValue.Expanded) {
+                                Icons.Default.KeyboardArrowUp // Раскрыт -> стрелка вверх (тяни вверх ещё)
+                            } else {
+                                Icons.Default.KeyboardArrowDown
+                            },
+                        // Сжат -> стрелка вниз (тяни вниз)
+                        contentDescription = "Свайп",
+                        modifier = Modifier.size(24.dp),
+                    )
+                }
+            }
+        },
+        sheetPeekHeight = 58.dp,
+        sheetSwipeEnabled = true,
     ) { innerPadding ->
-        CreateScreenContent(innerPadding)
+        Scaffold(
+            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+            topBar = { CreateScreenTopBar(scrollBehavior) },
+        ) { scaffoldInnerPadding ->
+            CreateScreenContent(scaffoldInnerPadding)
+        }
     }
 }
 
