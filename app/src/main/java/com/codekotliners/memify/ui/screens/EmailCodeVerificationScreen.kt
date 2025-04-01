9520d495b708d2c5a4456b070a376cbd2d148db9
mareliberum <vladfokin646@gmail.com>
2025-04-01 21:49:42 +0300

    косметика

--- app/src/main/java/com/codekotliners/memify/ui/screens/EmailCodeVerificationScreen.kt
+++ app/src/main/java/com/codekotliners/memify/ui/screens/EmailCodeVerificationScreen.kt
@@ -6,9 +6,9 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -182,7 +182,7 @@ fun CodeFromEmailForm(
             modifier =
                 Modifier
                     .fillMaxWidth()
-                    .height(52.dp),
+                    .wrapContentHeight(),
             shape = RoundedCornerShape(16.dp),
             colors =
                 ButtonDefaults.buttonColors(
