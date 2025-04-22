d563f3b8fc68b7d11ca019eeea2b54e1c0be668e
mareliberum <vladfokin646@gmail.com>
2025-04-22 14:19:50 +0300

    deleted unused imports

--- app/src/main/java/com/codekotliners/memify/MainActivity.kt
+++ app/src/main/java/com/codekotliners/memify/MainActivity.kt
@@ -6,11 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.codekotliners.memify.core.theme.MemifyTheme
 
-import com.codekotliners.memify.features.create.presentation.ui.CreateScreen
-
-import com.codekotliners.memify.features.home.presentation.ui.HomeScreen
-import com.codekotliners.memify.features.profile.presentation.ui.ProfileScreen
-
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
