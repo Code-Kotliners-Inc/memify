77863f6d219b6536c9fedd2a101734585c8d0296
mareliberum <vladfokin646@gmail.com>
2025-04-22 14:04:32 +0300

    репозиторий шаблонов

---
+++ app/src/main/java/com/codekotliners/memify/core/models/Template.kt
@@ -0,0 +1,9 @@
+package com.codekotliners.memify.core.models
+
+data class Template(
+    val templateId: String,
+    val name: String,
+    val templateUrl: String,
+    val favouritesCount : Int,
+
+)
