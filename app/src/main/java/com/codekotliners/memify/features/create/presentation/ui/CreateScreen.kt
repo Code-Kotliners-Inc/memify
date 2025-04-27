fd5186cf328baa1f03cba7674da55ac14beabe1a
IvanCRA <101933328+IvanCRA@users.noreply.github.com>
2025-04-27 12:23:29 +0300

    fix ktlint

--- app/src/main/java/com/codekotliners/memify/features/create/presentation/ui/CreateScreen.kt
+++ app/src/main/java/com/codekotliners/memify/features/create/presentation/ui/CreateScreen.kt
@@ -3,9 +3,7 @@ package com.codekotliners.memify.features.create.presentation.ui
 import android.content.res.Configuration
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectTapGestur
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
