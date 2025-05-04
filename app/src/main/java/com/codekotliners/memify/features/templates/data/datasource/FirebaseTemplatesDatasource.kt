c8884968c32ee9858caeb4a67c527fe95837f374
Fetisov Igor <87705301+Fetisyony@users.noreply.github.com>
2025-05-04 23:36:17 +0300

    Implementing templates feed paging feature (#73)
    * Add limit parameter
    
    * Implement paging

--- app/src/main/java/com/codekotliners/memify/features/templates/data/datasource/FirebaseTemplatesDatasource.kt
+++ app/src/main/java/com/codekotliners/memify/features/templates/data/datasource/FirebaseTemplatesDatasource.kt
@@ -7,9 +7,11 @@ import com.codekotliners.memify.features.templates.data.constants.FIELD_TEMPLATE
 import com.codekotliners.memify.features.templates.data.constants.FIELD_TEMPLATE_USED_COUNT
 import com.codekotliners.memify.features.templates.data.constants.TEMPLATES_COLLECTION_NAME
 import com.codekotliners.memify.features.templates.data.mappers.toTemplate
+import com.codekotliners.memify.features.templates.domain.datasource.DatasourceResult
 import com.codekotliners.memify.features.templates.domain.datasource.TemplatesDatasource
-import com.codekotliners.memify.features.templates.domain.datasource.TemplatesType
+import com.codekotliners.memify.features.templates.domain.datasource.TemplatesFilter
 import com.google.firebase.Firebase
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.firestore
 import kotlinx.coroutines.flow.Flow
@@ -17,40 +19,78 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
-class FirebaseTemplatesDatasource @Inject constructor() : TemplatesDatasource {
+class FirebaseTemplatesDatasource @Inject constructor() : TemplatesDatasource<DocumentSnapshot> {
     private val db = Firebase.firestore
     private val templatesCollection = db.collection(TEMPLATES_COLLECTION_NAME)
 
-    override suspend fun getFilteredTemplates(type: TemplatesType): Flow<Template> {
+    override suspend fun getFilteredTemplates(
+        type: TemplatesFilter,
+        limit: Long,
+        startWith: DocumentSnapshot?,
+    ): DatasourceResult<DocumentSnapshot> {
+        // to handle paging to get next document after last returned
+        val limitToFetch = limit + 1
         val queryByType =
             when (type) {
-                is TemplatesType.BEST -> queryBest()
-                is TemplatesType.FAVOURITES -> queryFavourites(type.userId)
-                is TemplatesType.NEW -> queryNew()
+                is TemplatesFilter.Best -> queryBest(limitToFetch, startWith)
+                is TemplatesFilter.New -> queryNew(limitToFetch, startWith)
+                is TemplatesFilter.Favorites -> queryFavourites(type.userId, limitToFetch, startWith)
             }
 
-        return fetchTemplates(queryByType)
+        val snap = queryByType.get().await()
+        val nextToStart =
+            if (snap.documents.size.toLong() == limit + 1) {
+                snap.documents.last()
+            } else {
+                null
+            }
+        return DatasourceResult(
+            fetchTemplates(snap.documents.dropLast(1)),
+            nextToStart,
+        )
     }
 
-    private fun fetchTemplates(query: Query): Flow<Template> =
+    private fun fetchTemplates(documents: List<DocumentSnapshot>): Flow<Template> =
         flow {
-            val snap = query.get().await()
-            snap.documents.forEach { doc ->
+            documents.forEach { doc ->
                 try {
                     emit(doc.toTemplate())
                 } catch (e: Exception) {
                     Logger.log(Logger.Level.ERROR, "Templates parsing", "For document: ${doc.id} ${e.message}")
-                    null
                 }
             }
         }
 
-    private fun queryNew(): Query =
-        templatesCollection.orderBy(FIELD_TEMPLATE_CREATED_AT, Query.Direction.DESCENDING)
+    private fun queryBest(limit: Long, startAtDocument: DocumentSnapshot?): Query {
+        var baseQuery =
+            templatesCollection
+                .orderBy(FIELD_TEMPLATE_USED_COUNT, Query.Direction.DESCENDING)
+                .limit(limit)
+        if (startAtDocument != null) {
+            baseQuery = baseQuery.startAt(startAtDocument)
+        }
+        return baseQuery
+    }
 
-    private fun queryBest(): Query =
-        templatesCollection.orderBy(FIELD_TEMPLATE_USED_COUNT, Query.Direction.DESCENDING)
+    private fun queryNew(limit: Long, startAtDocument: DocumentSnapshot?): Query {
+        var baseQuery =
+            templatesCollection
+                .orderBy(FIELD_TEMPLATE_CREATED_AT, Query.Direction.DESCENDING)
+                .limit(limit)
+        if (startAtDocument != null) {
+            baseQuery = baseQuery.startAt(startAtDocument)
+        }
+        return baseQuery
+    }
 
-    private fun queryFavourites(userId: String): Query =
-        templatesCollection.whereArrayContains(FIELD_TEMPLATE_FAVOURITED_BY_COUNT, userId)
+    private fun queryFavourites(userId: String, limit: Long, startAtDocument: DocumentSnapshot?): Query {
+        var baseQuery =
+            templatesCollection
+                .whereArrayContains(FIELD_TEMPLATE_FAVOURITED_BY_COUNT, userId)
+                .limit(limit)
+        if (startAtDocument != null) {
+            baseQuery = baseQuery.startAt(startAtDocument)
+        }
+        return baseQuery
+    }
 }
--- app/src/main/java/com/codekotliners/memify/features/templates/domain/repository/TemplatesRepository.kt
+++ app/src/main/java/com/codekotliners/memify/features/templates/domain/repository/TemplatesRepository.kt
@@ -4,9 +4,9 @@ import com.codekotliners.memify.core.models.Template
 import kotlinx.coroutines.flow.Flow
 
 interface TemplatesRepository {
-    suspend fun getBestTemplates(): Flow<Template>
+    suspend fun getBestTemplates(limit: Long, refresh: Boolean = false): Flow<Template>
 
-    suspend fun getNewTemplates(): Flow<Template>
+    suspend fun getNewTemplates(limit: Long, refresh: Boolean = false): Flow<Template>
 
-    suspend fun getFavouriteTemplates(): Flow<Template>
+    suspend fun getFavouriteTemplates(limit: Long, refresh: Boolean = false): Flow<Template>
 }
