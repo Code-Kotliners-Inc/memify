28a6f0681b50c0bfe9dbf619de34dd618f32a2aa
IvanCRA <101933328+IvanCRA@users.noreply.github.com>
2025-05-27 18:06:26 +0300

    fix code

--- app/src/main/java/com/codekotliners/memify/core/repositories/UriRepository.kt
+++ app/src/main/java/com/codekotliners/memify/core/repositories/UriRepository.kt
@@ -2,6 +2,9 @@ package com.codekotliners.memify.core.repositories
 
 import com.codekotliners.memify.core.database.dao.UriDao
 import com.codekotliners.memify.core.database.entities.UriEntity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -10,8 +13,13 @@ class UriRepository @Inject constructor(
     private val uriDao: UriDao,
 ) {
     suspend fun saveUri(uri: String) {
-        uriDao.insertUri(UriEntity(uri = uri))
+        withContext(Dispatchers.IO) {
+            uriDao.insertUri(UriEntity(uri = uri))
+        }
     }
 
-    fun getAllUris() = uriDao.getAllUris()
+    suspend fun getAllUris(): Flow<List<UriEntity>> =
+        withContext(Dispatchers.IO) {
+            uriDao.getAllUris()
+        }
 }
