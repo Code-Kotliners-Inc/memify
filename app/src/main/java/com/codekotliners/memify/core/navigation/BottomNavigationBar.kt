1979e22fe3d869c450c643d7f43ff24789eaf62c
mareliberum <vladfokin646@gmail.com>
2025-05-29 20:04:55 +0300

    hot fix nav bar borders

--- app/src/main/java/com/codekotliners/memify/core/navigation/BottomNavigationBar.kt
+++ app/src/main/java/com/codekotliners/memify/core/navigation/BottomNavigationBar.kt
@@ -1,7 +1,5 @@
 package com.codekotliners.memify.core.navigation
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
@@ -13,6 +11,8 @@ import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -26,9 +26,16 @@ fun BottomNavigationBar(navController: NavController) {
     NavigationBar(
         modifier =
             Modifier
-                .border(BorderStroke(1.dp, Color.LightGray))
                 .height(60.dp)
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .drawBehind {
+                    drawLine(
+                        color = Color.LightGray,
+                        start = Offset(0f, 0f),
+                        end = Offset(size.width, 0f),
+                        strokeWidth = 1.dp.toPx(),
+                    )
+                },
         containerColor = MaterialTheme.colorScheme.surface,
         contentColor = MaterialTheme.colorScheme.onSurface,
     ) {
